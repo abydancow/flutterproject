7cb58066e7bc734da93b5941eee38b209b269004
aby <syafrillahastro@gmail.com>
2024-04-25 11:44:10 +0700

    ubah nilai akhir

--- lib/nilai_akhir.dart
+++ lib/nilai_akhir.dart
@@ -8,12 +8,89 @@ class NilaiAkhir extends StatefulWidget {
 }
 
 class _NilaiAkhirState extends State<NilaiAkhir> {
+  String? nilaiAkhirHuruf;
+  double? nilaiRataRata;
+
+  TextEditingController? inputNilaiTugas = TextEditingController();
+  TextEditingController? inputNilaiUTS = TextEditingController();
+  TextEditingController? inputNilaiUAS = TextEditingController();
+
+  hitungNilai() {
+    setState(() {
+      int nilai1 = int.parse(inputNilaiTugas?.text ?? '0');
+      int nilai2 = int.parse(inputNilaiUTS?.text ?? '0');
+      int nilai3 = int.parse(inputNilaiUAS?.text ?? '0');
+      nilaiRataRata = (nilai1 + nilai2 + nilai3) / 3;
+
+      konversiHuruf(nilaiRataRata ?? 0);
+    });
+  }
+
+  konversiHuruf(double nilai) {
+    if (nilai >= 90 && nilai <= 100) {
+      nilaiAkhirHuruf = "nilai A";
+    } else if (nilai >= 70 && nilai <= 89) {
+      nilaiAkhirHuruf = "nilai B";
+    } else if (nilai >= 50 && nilai <= 69) {
+      nilaiAkhirHuruf = "nilai C";
+    } else {
+      nilaiAkhirHuruf = "belum lulus";
+    }
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("kontrol"),
-        backgroundColor: Colors.blueGrey,
+        title: Text("Nilai akhir"),
+        backgroundColor: Colors.amber,
+      ),
+      body: Column(
+        children: [
+          Text(nilaiAkhirHuruf ?? "nilai akhir"),
+          SizedBox(
+            height: 30,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: "masukan nilai tugas",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15))),
+            controller: inputNilaiTugas,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: "masukan nilai UTS",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15))),
+            controller: inputNilaiUTS,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: "masukan nilai UAS",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15))),
+            controller: inputNilaiUAS,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                hitungNilai();
+              },
+              child: Text("hitung nilai")),
+          Text("nilai rata rata:"),
+          Text(nilaiRataRata?.toString() ?? "0"),
+        ],
       ),
     );
   }
